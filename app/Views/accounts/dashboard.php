6219007e925d7d31a577eed513f14625c434a0b0
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-09 17:38:49 +0800

    Added dashboard file

---
+++ app/Views/accounts/dashboard.php
@@ -0,0 +1,8 @@
+<div class="container mt-5">
+    <h2 class="text-center">Dashboard</h2>
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <p>Welcome, <?= $username ?>!</p>
+        </div>
+    </div>
+</div>
