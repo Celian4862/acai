a1d7bf3eb1e74cad3815e7df3f1c557207d14b89
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-09 17:31:53 +0800

    Added redirect functionalities

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,8 +6,11 @@ use CodeIgniter\Exceptions\PageNotFoundException;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
+        if (session()->has('logged_in') && session()->get('logged_in') === true) {
+            return redirect()->to('accounts/dashboard');
+        }
         return view('templates/header', ['title' => 'Adaptive Community-Assisted Infrastructure'])
             . view('components/nav')
             . view('pages/home')
@@ -15,6 +18,9 @@ class Home extends BaseController
     }
 
     public function view(string $page) {
+        if (session()->has('logged_in') && session()->get('logged_in') === true) {
+            return redirect()->to('accounts/dashboard');
+        }
         if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
             throw new PageNotFoundException($page);
         }
