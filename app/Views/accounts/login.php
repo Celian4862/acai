633dda3d37c04f3a1e9071cf27819e5b880c5720
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-09 17:37:12 +0800

    Edited hyperlinks, edited error message display

--- app/Views/accounts/login.php
+++ app/Views/accounts/login.php
@@ -7,29 +7,25 @@
         <div class="col justify-content-center">
             <div class = "m-5">
                 <h2>Log In</h2>
-                <form action="/login" method="POST">
+                <form action="/accounts/login" method="POST">
                     <?= csrf_field() ?>
                     <div class="mb-3">
-                        <label for="email" class="form-label">Email address / Username</label>
-                        <input type="text" class="form-control" id="name_email" name="name_email" placeholder="Enter your email or username" required value="<?= $_SESSION['name_email'] ?? ''; ?>">
-                        <?php if (isset($_SESSION["invalid_user"])) { ?>
-                            <div class="form-text text-danger">User not found.</div>
-                        <?php } ?>
+                        <label for="name-email" class="form-label">Email address / Username</label>
+                        <input type="text" class="form-control" id="name-email" name="name-email" placeholder="Enter your email or username" required value="<?= set_value('name-email') ?>">
                     </div>
+                    
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password">
-                        <!-- DISPLAY ERROR MESSAGE -->
-                        <?php if (isset($_SESSION["invalid_pass"])) { ?>
-                            <div class="form-text text-danger">Wrong password.</div>
-                        <?php } ?>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password" value="<?= set_value('password') ?>">
                     </div>
+
                     <div class="d-flex justify-content-between">
                         <button type="submit" class="btn btn-primary">Log In</button>
-                        <a href="/signup" class="btn btn-outline-secondary">Create account</a>
+                        <a href="/accounts/signup" class="btn btn-outline-secondary">Create account</a>
                     </div>
+
                     <div class="text-center mt-3">
-                        <a href="/forgot-password">Forgot password?</a>
+                        <a href="/accounts/forgot-password">Forgot password?</a>
                     </div>
                 </form>
             </div>
