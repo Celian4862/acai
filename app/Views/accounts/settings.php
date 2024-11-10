644a2fa9997d8e8f13eb8896b1a12bd4755da116
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-10 23:27:12 +0800

    Edited settings.php Updated the error message display to display custom error messages if there are more errors after the first set of validation rules were passed.
    Changed the way the form block was opened and added the csrf_field() function, fixing the SecurityException bug.
    
    Made some fields required by default as well as added code to dynamically display certain values in the fields.

--- app/Views/accounts/settings.php
+++ app/Views/accounts/settings.php
@@ -1,36 +1,52 @@
+<?php helper('form'); ?>
 <div class="container mt-5">
     <h2 class="text-center">User Settings</h2>
     <div class="row justify-content-center">
         <div class="col-md-4">
-            <form action="/accounts/settings" method="POST">
+
+            <div class="text-danger">
+                <?= session()->getFlashdata('error') ?>
+                <?= validation_list_errors() ?>
+                <?php
+                    if (session()->getFlashdata('custom_errors')) {
+                        echo "<ul>";
+                        foreach (session()->getFlashdata('custom_errors') as $error) {
+                            echo "<li>{$error}</li>\n";
+                        }
+                        session()->remove('custom_errors');
+                    }
+                ?>
+            </div>
+
+            <?= form_open('accounts/settings') ?>
+                <?= csrf_field() ?>
                 <div class="mb-3">
                     <label for="email" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="email" name="email" value="<?= session()->get('email') ?>" />
+                    <input type="email" class="form-control" id="email" name="email" required value="<?= ($_SERVER['REQUEST_METHOD'] === 'GET') ? session()->get('email') : set_value('email'); ?>" />
                 </div>
                 <div class="mb-3">
                     <label for="username" class="form-label">Username</label>
-                    <input type="username" class="form-control" id="username" name="username" value="<?= session()->get('username') ?>" />
+                    <input type="username" class="form-control" id="username" name="username" required value="<?= ($_SERVER['REQUEST_METHOD'] === 'GET') ? session()->get('username') : set_value('username') ?>" />
                 </div>
                 <div class="mb-3">
                     <label for="old-pass" class="form-label">Old password</label>
-                    <input type="password" class="form-control" id="old-pass" name="old-pass" />
+                    <input type="password" class="form-control" id="old-password" name="old-password" value="<?= set_value('old-password') ?>" />
                 </div>
                 <div class="mb-3">
                     <label for="new-pass" class="form-label">New password</label>
-                    <input type="password" class="form-control" id="new-pass" name="new-pass" />
+                    <input type="password" class="form-control" id="new-password" name="new-password" value="<?= set_value('new-password') ?>" />
                 </div>
                 <div class="mb-3">
                     <label for="conf-new-pass" class="form-label">Confirm new password</label>
-                    <input type="password" class="form-control" id="conf-new-pass" name="conf-new-pass" />
+                    <input type="password" class="form-control" id="confirm-password" name="confirm-password" value="<?= set_value('confirm-password') ?>" />
                 </div>
                 <div class="mb-3">
                     <label for="birthdate" class="form-label">Date of Birth</label>
-                    <input type="date" class="form-control" id="birthdate" name="birthdate" value="<?= session()->get('birthdate') ?>" />
+                    <input type="date" class="form-control" id="birthdate" name="birthdate" required value="<?= ($_SERVER['REQUEST_METHOD'] === 'GET') ? session()->get('birthdate') : set_value('birthdate') ?>" />
                 </div>
                 <div class="d-flex justify-content-between">
                     <button type="submit" class="btn btn-primary">Update</button>
                 </div>
-            </form>
         </div>
     </div>
 </div>
