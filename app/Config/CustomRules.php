958a8a759cc55f4d8e06f86c845f81f242b4f619
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-11 22:22:18 +0800

    Added custom rules for form validation

---
+++ app/Config/CustomRules.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Config;
+
+use App\Models\AccountsModel;
+
+class CustomRules
+{
+    /**
+     * Checks if the inputted email is unique
+     * @param mixed $value
+     * @return bool
+     */
+    public function unique_email($value) {
+        $model = model(AccountsModel::class); // Initialise the model instance
+
+        return $value === session()->get('email') || $model->getAccount($value) === null; // Return true if the email is the same as the current user's email or if the email is not found in the database
+    }
+
+    /**
+     * Checks if the inputted username is unique
+     * @param mixed $value
+     * @return bool
+     */
+    public function unique_username($value) {
+        $model = model(AccountsModel::class); // Initialise the model instance
+
+        return $value === session()->get('username') || $model->getAccount($value) === null; // Return true if the username is the same as the current user's username or if the username is not found in the database
+    }
+    
+    /**
+     * Checks if the password is decent
+     * @param mixed $value
+     * @return bool
+     */
+    public function password_ok($value) {
+        return strlen($value) < 8 || !preg_match("/^(?=.*\w)(?=.*\d)(?=.*\W)[\w\d\W]{8,}$/", $value) && strlen($value) < 16;
+    }
+
+    /**
+     * Checks if the inputted password does not match the current password of the current user
+     * @param mixed $value
+     * @return bool
+     */
+    public function no_match_old_pass($value) {
+        $model = model(AccountsModel::class); // Initialise the model instance
+
+        $account = $model->getAccount(session()->get('username')); // Get the current user's account
+
+        return ! password_verify($value, $account['password']); // Return true if the inputted password matches the current user's password
+    }
+
+    /**
+     * Checks if the password matches the current password of the current user
+     * @param mixed $value
+     * @return bool
+     */
+    public function correct_password($value) {
+        $model = model(AccountsModel::class);
+
+        $account = $model->getAccount(session()->get('username'));
+
+        return password_verify($value, $account['password']);
+    }
+
+    
+}
