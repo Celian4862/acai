3e08a167b0720681b27be34f78a0328fdaee4724
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-11 23:38:55 +0800

    Added new rulesets alongside custom rules

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -25,6 +25,7 @@ class Validation extends BaseConfig
         FormatRules::class,
         FileRules::class,
         CreditCardRules::class,
+        CustomRules::class,
     ];
 
     /**
@@ -42,44 +43,126 @@ class Validation extends BaseConfig
     // Rules
     // --------------------------------------------------------------------
 
-    public array $user_settings = [
+    public array $login = [
+        'name-email' => [
+            'label' => 'Username / Email',
+            'rules' => 'required|min_length[6]|max_length[30]',
+            'errors' => [
+                'required' => '{field} is required.',
+                'min_length' => '{field} must be at least {param} characters.',
+                'alpha_numeric_punct' => '{field} must contain only letters, numbers, and symbols.',
+                'max_length' => '{field} may contain up to {param} characters.',
+            ],
+        ],
+        'password' => [
+            'label' => 'Password',
+            'rules' => 'required|min_length[8]|max_length[255]|name_email_exists[name-email]|correct_password[name-email]',
+            'errors' => [
+                'required' => '{field} is required.',
+                'min_length' => '{field} must be at least {param} characters.',
+                'max_length' => '{field} may contain up to {param} characters.',
+                'name_email_exists' => 'User does not exist.',
+                'correct_password' => 'Incorrect password.',
+            ],
+        ],
+    ];
+    
+    /**
+     * Ruleset for signup form
+     * @var array
+     */
+    public array $signup = [
         'email' => [
             'label' => 'Email',
-            'rules' => 'required|valid_email',
+            'rules' => 'required|valid_email|is_unique[accounts.email]',
             'errors' => [
+                'required' => '{field} is required.',
                 'valid_email' => 'Email is not valid.',
-            ]
+                'is_unique' => 'Email is already in use.',
+            ],
         ],
         'username' => [
             'label' => 'Username',
-            'rules' => 'required|alpha_numeric|max_length[30]',
+            'rules' => 'required|alpha_numeric|min_length[6]|max_length[30]',
             'errors' => [
                 'required' => '{field} is required.',
                 'alpha_numeric' => '{field} must contain only letters and numbers.',
                 'max_length' => '{field} may contain up to {param} characters.',
             ],
         ],
-        'old-pass' => [
-            'label' => 'Old password',
-            'rules' => 'field_exists|permit_empty|min_length[8]|max_length[255]',
+        'password' => [
+            'label' => 'Password',
+            'rules' => 'required|min_length[8]|max_length[255]',
             'errors' => [
-                'min_length' => '{field} must be at least {param} characters.',
-                'max_length' => '{field} may contain up to {param} characters.',
-            ],
-        ],
-        'new-pass' => [
-            'label' => 'New password',
-            'rules' => 'permit_empty|min_length[8]|max_length[255]',
-            'errors' => [
+                'required' => '{field} is required.',
                 'min_length' => '{field} must be at least {param} characters.',
                 'max_length' => '{field} may contain up to {param} characters.',
             ],
         ],
         'confirm-pass' => [
             'label' => 'Password confirmation',
+            'rules' => 'required|matches[password]',
+            'errors' => [
+                'matches' => 'Passwords do not match.',
+            ],
+        ],
+        'birthdate' => [
+            'label' => 'Date of birth',
+            'rules' => 'required|valid_date',
+            'errors' => [
+                'required' => '{field} is required.',
+                'valid_date' => '{field} must be valid.',
+            ],
+        ],
+    ];
+
+    /**
+     * Ruleset for user settings form
+     * @var array
+     */
+    public array $user_settings = [
+        'email' => [
+            'label' => 'Email',
+            'rules' => 'required|valid_email|unique_email',
+            'errors' => [
+                'valid_email' => 'Email is not valid.',
+                'unique_email' => 'Email is already in use.',
+            ],
+        ],
+        'username' => [
+            'label' => 'Username',
+            'rules' => 'required|alpha_numeric|max_length[30]|unique_username',
+            'errors' => [
+                'required' => '{field} is required.',
+                'alpha_numeric' => '{field} must contain only letters and numbers.',
+                'max_length' => '{field} may contain up to {param} characters.',
+                'unique_username' => '{field} has been taken.',
+            ],
+        ],
+        'old-pass' => [
+            'label' => 'Old password',
+            'rules' => 'permit_empty|min_length[8]|max_length[255]|correct_password[username]',
+            'errors' => [
+                'min_length' => '{field} must be at least {param} characters.',
+                'max_length' => '{field} may contain up to {param} characters.',
+                'correct_password' => 'Incorrect password.',
+            ],
+        ],
+        'new-pass' => [
+            'label' => 'New password',
+            'rules' => 'permit_empty|min_length[8]|max_length[255]|no_match_old_pass|password_ok',
+            'errors' => [
+                'min_length' => '{field} must be at least {param} characters.',
+                'max_length' => '{field} may contain up to {param} characters.',
+                'no_match_old_pass' => 'New password must be different from the old password.',
+                'password_ok' => '{field} must contain at least one letter, number, and symbol.',
+            ],
+        ],
+        'confirm-pass' => [
+            'label' => 'Password confirmation',
             'rules' => 'matches[new-pass]',
             'errors' => [
-                ''
+                'matches' => 'Passwords do not match.',
             ],
         ],
         'birthdate' => [
