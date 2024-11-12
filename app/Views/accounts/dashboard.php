175503771f34a66357fc915a42187d36ccd35972
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-12 20:38:40 +0800

    Escaped data

--- app/Views/accounts/dashboard.php
+++ app/Views/accounts/dashboard.php
@@ -2,7 +2,7 @@
     <h2 class="text-center">Dashboard</h2>
     <div class="row justify-content-center">
         <div class="col-md-4">
-            <p>Welcome, <?= $username ?>!</p>
+            <p>Welcome, <?= esc($username) ?>!</p>
         </div>
     </div>
 </div>
