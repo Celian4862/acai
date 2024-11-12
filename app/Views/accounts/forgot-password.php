0ba9edf3aa7fb1e6d0818f89fe02d39be862604e
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-12 21:03:50 +0800

    Refactored forgot-password.php to use the form helper

--- app/Views/accounts/forgot-password.php
+++ app/Views/accounts/forgot-password.php
@@ -2,21 +2,23 @@
     <h2 class="text-center">Forgot Password</h2>
     <div class ="row justify-content-center">
         <div class="col-md-4">
-            <form action="/accounts/forgot-password" method="post">
+            <?= form_open('accounts/forgot-password') ?>
                 <?= csrf_field() ?>
 
                 <div class="mb-3">
-                    <label for="email">Email address:</label>
-                    <input type="email" class="form-control" id="email" name="email" required value="<?= set_value('email') ?>">
+                    <?= form_label('Email', 'email', ['class' => 'form-label']) ?>
+                    <?= form_input('email', set_value('email', '', false), ['id' => 'email', 'class' => 'form-control', 'required' => true], 'email') ?>
 
                     <div class="text-danger">
-                        <?= session()->getFlashdata('error') ?>
                         <?= validation_list_errors() ?>
                     </div>
 
+                    <div class="text-success text-center">
+                        <?= session()->getFlashdata('success') ?>
+                    </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+                <?= form_submit('submit', 'Submit', ['class' => 'btn btn-primary']) ?>
+            <?= form_close() ?>
         </div>
     </div>
 </div>
