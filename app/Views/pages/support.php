b07b9b60b15088aaca7edecaa3cf6fc75d179097
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-23 17:56:12 +0800

    Removed support.php and made it into the footer.

--- app/Views/pages/support.php
+++
@@ -1,7 +0,0 @@
-<div class="text-center">
-    <h1>Need assistance?</h1>
-    <p style="font-size: 150%;">Contact our support team using the details below!</p>
-    <ul id="support-list">
-        <li>support@acai.com
-    </ul>
-</div>
--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,2 +1,7 @@
-    </body>
+    <footer class="mt-auto p-4" style="background-color: #8DB600;">
+      <div class="d-flex justify-content-center">
+        <span class="text-center">Contact us at: support@acai.com</span>
+      </div>
+    </footer>
+  </body>
 </html>
