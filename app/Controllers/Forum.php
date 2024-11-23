134a798e84805117737107a5f8fafb3e942e152c
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-23 18:12:47 +0800

    Modified the controller The view() function now can retrieve posts from the database and pass the data to the dashboard view.

--- app/Controllers/Forum.php
+++ app/Controllers/Forum.php
@@ -3,21 +3,31 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\ForumModel;
+use CodeIgniter\Exceptions\PageNotFoundException;
 
 class Forum extends BaseController
 {
-    public function view($page = 'dashboard') {
-        if (!is_file(APPPATH.'/Views/forum/'.$page.'.php')) {
-            throw new PageNotFoundException($page);
-        }
+public function view($page = 'dashboard') {
+  if (!is_file(APPPATH.'/Views/forum/'.$page.'.php')) {
+    throw new PageNotFoundException($page);
+  }
 
-        if (session()->has('logged_in') && session()->get('logged_in') === true) {
-            return view('templates/header', ['title' => ucwords(str_replace('-', ' ', $page))])
-            . view('components/nav')
-            . view('forum/' . $page, session()->get())
-            . view('templates/footer');
-        }
+  if (session()->has('logged_in') && session()->get('logged_in') === true) {
+    $seeder = \Config\Database::seeder();
+    $seeder->call('PostsTable');
+    
+    $model = model(ForumModel::class);
+    $posts = $model->getPosts();
+    $data = session()->get();
+    $data['posts'] = $posts;
 
-        return redirect('Accounts::view');
+    return view('templates/header', ['title' => ucwords(str_replace('-', ' ', $page))])
+      . view('components/nav')
+      . view('forum/' . $page, $data)
+      . view('templates/footer');
     }
+  
+    return redirect('Accounts::view');
+  }
 }
