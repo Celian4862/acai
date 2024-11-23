ab45aebcd5ae47264307a9227936247e66678901
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-23 18:13:08 +0800

    dashboard.php can now display posts.

--- app/Views/forum/dashboard.php
+++ app/Views/forum/dashboard.php
@@ -1,8 +1,21 @@
 <div class="container mt-5">
-    <h2 class="text-center">Dashboard</h2>
+  <h2 class="text-center">Dashboard</h2>
+  <?php foreach ($posts as $post): ?>
     <div class="row justify-content-center">
-        <div class="col-md-4">
-            <p>Welcome, <?= esc($username) ?>!</p>
+      <div class="col-md-4">
+        <div class="card mt-3">
+          <div class="card-header">
+            <h5 class="card-title"><?= esc($post['title']) ?></h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text"><?= esc($post['body']) ?></p>
+          </div>
+          <div class="card-footer">
+            <!-- This doesn't work yet. -->
+            <a href="/forum/posts/<?= esc($post['slug'], 'url') ?>" class="btn btn-primary">View Post</a>
+          </div>
         </div>
+      </div>
     </div>
+  <?php endforeach ?>
 </div>
