b48b866c92b65009761ffb0894a1213d3f487366
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-26 11:24:20 +0800

    Transferred some rules to Accounts.php due to being used only once

--- app/Controllers/Accounts.php
+++ app/Controllers/Accounts.php
@@ -47,13 +47,33 @@ class Accounts extends BaseController
         $seeder = \Config\Database::seeder();
         $seeder->call('AccountsTable');
 
-        $validator = service('validation');
-
-        if (! $validator->run($data, 'login')) {
+        if (! $this->validateData($data, [
+            'name-email' => [
+                'label' => 'Username / Email',
+                'rules' => 'required|min_length[6]|max_length[30]',
+                'errors' => [
+                    'required' => '{field} is required.',
+                    'min_length' => '{field} must be at least {param} characters.',
+                    'alpha_numeric_punct' => '{field} must contain only letters, numbers, and symbols.',
+                    'max_length' => '{field} may contain up to {param} characters.',
+                ],
+            ],
+            'password' => [
+                'label' => 'Password',
+                'rules' => 'required|min_length[8]|max_length[255]|name_email_exists[name-email]|correct_password[name-email]',
+                'errors' => [
+                    'required' => '{field} is required.',
+                    'min_length' => '{field} must be at least {param} characters.',
+                    'max_length' => '{field} may contain up to {param} characters.',
+                    'name_email_exists' => 'User does not exist.',
+                    'correct_password' => 'Incorrect password.',
+                ],
+            ],
+        ])) {
             return redirect()->to('/accounts/login')->withInput();
         }
 
-        $post = $validator->getValidated();
+        $post = $this->validator->getValidated();
 
         $model = model(AccountsModel::class);
 
@@ -80,13 +100,57 @@ class Accounts extends BaseController
 
         $data = $this->request->getPost(['email', 'username', 'password', 'confirm-pass', 'birthdate']);
 
-        $validator = service('validation');
-
-        if (! $validator->run($data, 'signup')) {
+        if (! $this->validateData($data, [
+            'email' => [
+                'label' => 'Email',
+                'rules' => 'required|valid_email|is_unique[accounts.email]',
+                'errors' => [
+                    'required' => '{field} is required.',
+                    'valid_email' => '{field} is not valid.',
+                    'is_unique' => '{field} is already in use.',
+                ],
+            ],
+            'username' => [
+                'label' => 'Username',
+                'rules' => 'required|alpha_numeric|min_length[6]|max_length[30]|is_unique[accounts.username]',
+                'errors' => [
+                    'required' => '{field} is required.',
+                    'alpha_numeric' => '{field} must contain only letters and numbers.',
+                    'min_length[6]' => '{field} must be at least six letters.',
+                    'max_length' => '{field} may contain up to {param} characters.',
+                    'is_unique' => '{field} is already in use.',
+                ],
+            ],
+            'password' => [
+                'label' => 'Password',
+                'rules' => 'required|min_length[8]|max_length[255]|password_ok',
+                'errors' => [
+                    'required' => '{field} is required.',
+                    'min_length' => '{field} must be at least {param} characters.',
+                    'max_length' => '{field} may contain up to {param} characters.',
+                    'password_ok' => '{field} must be at least 16 characters or be at least 8 characters and contain at least one letter, number, and symbol.',
+                ],
+            ],
+            'confirm-pass' => [
+                'label' => 'Password confirmation',
+                'rules' => 'required|matches[password]',
+                'errors' => [
+                    'matches' => '{field} does not match.',
+                ],
+            ],
+            'birthdate' => [
+                'label' => 'Date of birth',
+                'rules' => 'required|valid_date',
+                'errors' => [
+                    'required' => '{field} is required.',
+                    'valid_date' => '{field} must be valid.',
+                ],
+            ],
+        ])) {
             return redirect()->back()->withInput();
         }
 
-        $post = $validator->getValidated();
+        $post = $this->validator->getValidated();
 
         $model = model(AccountsModel::class);
 
@@ -147,13 +211,66 @@ class Accounts extends BaseController
 
         $data = $this->request->getPost(['email', 'username', 'old-pass', 'new-pass', 'confirm-pass', 'birthdate']);
 
-        $validation = service('validation');
-
-        if (! $validation->run($data, 'user_settings')) {
+        if (! $this->validateData($data, [
+            'email' => [
+                'label' => 'Email',
+                'rules' => 'required|valid_email|unique_email',
+                'errors' => [
+                    'valid_email' => '{field} is not valid.',
+                    'unique_email' => '{field} is already in use.',
+                ],
+            ],
+            'username' => [
+                'label' => 'Username',
+                'rules' => 'required|alpha_numeric|min_length[6]|max_length[30]|unique_username',
+                'errors' => [
+                    'required' => '{field} is required.',
+                    'alpha_numeric' => '{field} must contain only letters and numbers.',
+                    'max_length' => '{field} may contain up to {param} characters.',
+                    'unique_username' => '{field} has been taken.',
+                ],
+            ],
+            'old-pass' => [
+                'label' => 'Old password',
+                'rules' => 'permit_empty|required_with[new-pass]|min_length[8]|max_length[255]|correct_password[username]',
+                'errors' => [
+                    'min_length' => '{field} must be at least {param} characters.',
+                    'max_length' => '{field} may contain up to {param} characters.',
+                    'correct_password' => 'Incorrect password.',
+                ],
+            ],
+            'new-pass' => [
+                'label' => 'New password',
+                'rules' => 'required_with[old-pass]|permit_empty|min_length[8]|max_length[255]|differs[old-pass]|password_ok',
+                'errors' => [
+                    'required_with' => '{field} is required with {param}.',
+                    'min_length' => '{field} must be at least {param} characters.',
+                    'max_length' => '{field} may contain up to {param} characters.',
+                    'old_pass_correct' => 'Old password is incorrect or missing.',
+                    'no_match_old_pass' => '{field} must be different from old password.',
+                    'password_ok' => '{field} must be at least 16 characters or be at least 8 characters and contain at least one letter, number, and symbol.',
+                ],
+            ],
+            'confirm-pass' => [
+                'label' => 'Password confirmation',
+                'rules' => 'matches[new-pass]',
+                'errors' => [
+                    'matches' => 'Passwords do not match.',
+                ],
+            ],
+            'birthdate' => [
+                'label' => 'Date of birth',
+                'rules' => 'required|valid_date',
+                'errors' => [
+                    'required' => '{field} is required.',
+                    'valid_date' => '{field} must be valid.',
+                ],
+            ]
+        ])) {
             return redirect()->back()->withInput();
         }
 
-        $post = $validation->getValidated();
+        $post = $this->validator->getValidated();
 
         $model = model(AccountsModel::class);
 
