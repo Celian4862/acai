217272afbd461d8a83b57e4b005a89778a720998
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-27 13:49:37 +0800

    Completed the sentence in about-us.php

--- app/Views/pages/about-us.php
+++ app/Views/pages/about-us.php
@@ -2,5 +2,5 @@
     <h1>Build Your City, Your Way.</h1>
     <p id="tagline">Let everyone know where infrastructure development is needed.</p>
     <p>There has been a modern trend of large-scale unsustainable infrastructural projects catered more towards tourism and international appeal rather than livability and improvement. This has led to large spans of land that has either fallen into decay or left to collect dust due to a lack of either funding or attention. We believe that that has to change. This website intends that you will have a place to bring attention to any problems in your local area, whether it be cracked roads or overgrown trees, so that it may be dealt with accordingly to benefit the local populace.</p>
-    <p>In response to this, we created A&ccedil;a&iacute; &mdash; "Adaptive Community-Assisted Infrastructure" &mdash; </p>
+    <p>For this purpose, we created A&ccedil;a&iacute; &mdash; "Adaptive Community-Assisted Infrastructure" &mdash; a streamlined, intuitive, and most of all globally accessible way for community members to communicate the state of their local environment and to bring attention to the undeveloped parts of their daily life, ranging from abandoned malls and stations to cracked roads and uninhabitable infrastructure. </p>
 </div>
