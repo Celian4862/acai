8b95817ed9fe747dafca6c83b95fad249b921e6d
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-30 09:37:25 +0800

    Added form elements in app\Views\forum\newpost.php

--- app/Views/forum/newpost.php
+++ app/Views/forum/newpost.php
@@ -1,17 +1,32 @@
-<?php helper('form'); ?>
-<div class = "container mt-5">
-    <h2> Add new post </h2>
-    <div class = "row">
+<div class="container mt-5">
+    <h2>Add new post</h2>
+    <div class="row justify-content-center">
+        <?php helper('form'); ?>
         <div class="text-danger">
             <?= validation_list_errors() ?>
         </div>
-        <?= form_open('forum/newpost'); ?>
+    </div>
+    <?= form_open('forum/newpost', ['class' => 'row']); ?>
         <?= csrf_field() ?>
         <div class = "col-8">
-            <!-- textarea here for posts -->
+            <?= form_textarea([
+                'name' => 'body',
+                'id' => 'body',
+                'class' => 'form-control',
+                'placeholder' => 'Body'
+            ]); ?>
         </div>
         <div class = "col-4">
-            <!-- all the other post variables here -->
+            <?= form_input([
+                'name' => 'title',
+                'id' => 'title',
+                'class' => 'form-control',
+                'placeholder' => 'Title'
+            ]); ?>
+            <?= form_submit([
+                'value' => 'Add Post',
+                'class' => 'btn btn-primary mt-2'
+            ]); ?>
         </div>
-    </div>
+    <?= form_close(); ?>
 </div>
