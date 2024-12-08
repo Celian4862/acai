902f540eb35e5b169664bd9f62b3441c715de4cc
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-12-08 14:14:30 +0800

    Changed layout for adding new posts

--- app/Views/forum/newpost.php
+++ app/Views/forum/newpost.php
@@ -8,20 +8,18 @@
     </div>
     <?= form_open('forum/newpost', ['class' => 'row']); ?>
         <?= csrf_field() ?>
-        <div class = "col-8">
-            <?= form_textarea([
-                'name' => 'body',
-                'id' => 'body',
-                'class' => 'form-control',
-                'placeholder' => 'Body'
-            ]); ?>
-        </div>
-        <div class = "col-4">
+        <div class = "col">
             <?= form_input([
                 'name' => 'title',
                 'id' => 'title',
                 'class' => 'form-control',
                 'placeholder' => 'Title'
+            ]); ?>
+            <?= form_textarea([
+                'name' => 'body',
+                'id' => 'body',
+                'class' => 'form-control',
+                'placeholder' => 'Body'
             ]); ?>
             <?= form_submit([
                 'value' => 'Add Post',
