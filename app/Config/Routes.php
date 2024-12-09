d4b6c9cd115c2662e62e8a9fe8b90094c07ae4eb
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-12-09 12:25:37 +0800

    Updated reset-password.php files and Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,11 +16,13 @@ $routes->group('accounts', static function ($routes) {
     $routes->get('login', 'Accounts::view');
     $routes->get('signup', 'Accounts::view/signup');
     $routes->get('forgot-password', 'Accounts::view/forgot-password');
+    $routes->get('reset-password', 'Accounts::view/reset-password');
     $routes->get('logout', 'Accounts::logout');
     $routes->get('(:segment)', 'Accounts::user_views/$1');
     $routes->post('login', 'Accounts::login');
     $routes->post('signup', 'Accounts::create_account');
     $routes->post('forgot-password', 'Accounts::forgot_password');
+    $routes->post('reset-password', 'Accounts::reset_password');
     $routes->post('settings', 'Accounts::settings');
     $routes->post('delete-account', 'Accounts::delete_account');
 });
---
+++ app/Views/accounts/reset-password.php
@@ -0,0 +1,25 @@
+<div class="m-5">
+  <h2 class="text-center">Reset Password</h2>
+  <div class ="row justify-content-center">
+    <div class="col-md-4">
+      <?= form_open('accounts/reset-password') ?>
+        <?= csrf_field() ?>
+  
+        <div class="mb-3">
+          <?= form_label('New password', 'password', ['class' => 'form-label']) ?>
+          <?= form_input('password', '', ['id' => 'password', 'class' => 'form-control', 'required' => true], 'password') ?>
+        </div>
+
+        <div class="mb-3">
+          <?= form_label('Confirm password', 'confirm-password', ['class' => 'form-label']) ?>
+          <?= form_input('confirm-password', '', ['id' => 'confirm-password', 'class' => 'form-control', 'required' => true], 'password') ?>
+  
+          <div class="text-danger">
+            <?= validation_list_errors() ?>
+          </div>
+        </div>
+        <?= form_submit('submit', 'Submit', ['class' => 'btn btn-primary']) ?>
+      <?= form_close() ?>
+    </div>
+  </div>
+</div>
