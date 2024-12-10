5fd8c115ce77740c5682bf6f0e82a7adb6e378a3
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-12-10 23:07:45 +0800

    Added basic creation of posts

--- app/Controllers/Posts.php
+++ app/Controllers/Posts.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\AccountsModel;
 use App\Models\PostsModel;
 use CodeIgniter\Exceptions\PageNotFoundException;
 
@@ -29,6 +30,54 @@ class Posts extends BaseController
     }
 
     public function newpost() {
+        helper(['form', 'url']);
+
+        $data = $this->request->getPost(['title', 'body']);
+
+        if (! $this->validateData($data, [
+            'title' => [
+                'label' => 'Title',
+                'rules' => 'required|min_length[3]|max_length[255]',
+                'errors' => [
+                    'required' => '{field} is required',
+                    'min_length' => '{field} must be at least {param} characters',
+                    'max_length' => '{field} cannot exceed {param} characters'
+                ]
+            ],
+            'body' => [
+                'label' => 'Body',
+                'rules' => 'required|min_length[3]|max_length[1000]',
+                'errors' => [
+                    'required' => '{field} is required',
+                    'min_length' => '{field} must be at least {param} characters',
+                    'max_length' => '{field} cannot exceed {param} characters'
+                ]
+            ],
+        ])) {
+            return redirect()->back()->withInput();
+        }
+
+        $files = $this->request->getFiles();
+
+        if ($files) {
+            foreach ($files['images'] as $file) {
+                if ($file->isValid() && ! $file->hasMoved()) {
+                    $file->move(WRITEPATH . 'uploads');
+                }
+            }
+        }
+
+        $post = $this->validator->getValidated();
+
+        $model = model(PostsModel::class);
         
+        $model->save([
+            'title' => $post['title'],
+            'body' => $post['body'],
+            'slug' => url_title(strtolower($post['title'])),
+            'account_id' => model(AccountsModel::class)->getAccount(session()->get('username'))['id'],
+        ]);
+
+        return redirect()->to('/dashboard');
     }
 }
