6efcba0d48119db70e086f1128a7931fd92168f9
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-12-11 20:21:08 +0800

    Added app/Views/forum/index.php for a view of all posts

---
+++ app/Views/forum/index.php
@@ -0,0 +1,26 @@
+<div class="container mt-5">
+    <h2 class="text-center">Forum</h2>
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card mt-3 text-center">
+                <div class="card-body">
+                    <a href="/forum/newpost" class="btn btn-primary">+ Add new post</a>
+                </div>
+            </div>
+            <?php foreach ($posts as $post): ?>
+                <div class="card mt-3">
+                    <div class="card-header">
+                        <h5 class="card-title"><?= esc($post['title']) ?></h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text"><?= esc($post['body']) ?></p>
+                    </div>
+                    <div class="card-footer">
+                        <!-- This doesn't work yet. -->
+                        <a href="/forum/posts/<?= esc($post['slug'], 'url') ?>" class="btn btn-primary">View Post</a>
+                    </div>
+                </div>
+            <?php endforeach ?>
+        </div>
+    </div>
+</div>
