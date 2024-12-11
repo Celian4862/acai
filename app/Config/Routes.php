203c7ab6b5b07347a9f49bc91eb28490b5112604
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-12-11 21:43:58 +0800

    Rearranged routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,10 +10,12 @@ $routes->get('dashboard', 'Posts::dashboard');
 $routes->group('forum', static function ($routes) {
     $routes->get('/', 'Posts::view');
     $routes->get('(:segment)', 'Posts::view/$1');
-    $routes->get('posts/(:segment)', 'Posts::view_post/$1');
     $routes->post('newpost', 'Posts::newpost');
+    
+    $routes->get('posts/(:segment)', 'Posts::view_post/$1');
     $routes->post('posts/(:segment)', 'Posts::edit_post/$1');
     $routes->post('posts/(:segment)/delete', 'Posts::delete_post/$1');
+
     $routes->post('comment/(:segment)', 'Comments::add_comment/$1');
 });
 
