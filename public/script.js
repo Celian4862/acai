4a60c26b07ef5144e7025930353b6a152916bae0
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-12-12 14:14:28 +0800

    Added an alert message for uploading images, changed form upload layout

--- public/script.js
+++ public/script.js
@@ -9,18 +9,41 @@ document.addEventListener("DOMContentLoaded", function() {
 let pageNum = 0;
 
 function addFile() {
+    if (pageNum == 0) {
+        const newDiv1 = document.createElement('div');
+        newDiv1.id = `img-msg`;
+        newDiv1.className = 'alert alert-info';
+        newDiv1.innerHTML = `
+            <p>Image must be JPG, JPEG, or PNG.</p>
+        `;
+        document.getElementById('add-file').before(newDiv1);
+    }
     pageNum++;
     const newDiv = document.createElement('div');
     newDiv.id = `image${pageNum}`;
     newDiv.className = 'mb-3';
     newDiv.innerHTML = `
-        <input type="file" name="images[]" class="form-control" accept=".jpg, .jpeg, .png">
-        <button type="button" class="btn btn-secondary mt-2" onclick="removeFile('image${pageNum}')">Remove Image</button>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-10">
+                    <input type="file" name="images[]" class="form-control" accept=".jpg, .jpeg, .png">
+                </div>
+                <div class="col-md justify-content-center">
+                    <button type="button" class="btn btn-secondary" onclick="removeFile('image${pageNum}')">Remove Image</button>
+                </div>
+            </div>
+        </div>
     `;
-    document.getElementById('add-file').before(newDiv);
+    document.getElementById('add-file').after(newDiv);
 }
 
 function removeFile(elementId) {
+    if (pageNum == 1) {
+        const msg = document.getElementById('img-msg');
+        if (msg) {
+            msg.remove();
+        }
+    }
     const element = document.getElementById(elementId);
     if (element) {
         element.remove();
