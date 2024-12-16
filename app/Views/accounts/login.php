d56d8e2a4479bbae28e225d194766ccdd4d6535e
Yuhawari <aaronbenedictcaguin@gmail.com>
2024-12-16 10:15:32 +0800

    united the aesthetics of the signup and login pages

--- app/Views/accounts/login.php
+++ app/Views/accounts/login.php
@@ -1,37 +1,38 @@
-<div class="container mt-5">
-    <div class="row">
-        <div class="col login-bg"></div>
-        <div class="col justify-content-center">
-            <div class="m-5">
-                <h2>Log In</h2>
+<div class="container mt-5 p-0 m-0">
+        <div class="row login-bg justify-content-center vh-100 vw-100">
+            <div class="col"> </div>
+            <div class="col align-self-center field-bubble">
+                <div class="m-5">
+                    <h2>Log In</h2>
 
-                <div class="text-danger">
-                    <?= validation_list_errors() ?>
-                </div>
-                
-                <?= form_open('/login') ?>
-                    <?= csrf_field() ?>
-
-                    <div class="mb-3">
-                        <?= form_label('Email address / Username', 'name-email', ['class' => 'form-label']) ?>
-                        <?= form_input ('name-email', set_value('name-email', '', false), ['id' => 'name-email', 'class' => 'form-control', 'required' => true, 'placeholder' => 'Enter your email or username'], 'text') ?>
+                    <div class="text-danger">
+                        <?= validation_list_errors() ?>
                     </div>
                     
-                    <div class="mb-3">
-                        <?= form_label('Password', 'password', ['class' => 'form-label']) ?>
-                        <?= form_password('password', set_value('password', '', false), ['id' => 'password', 'class' => 'form-control', 'required' => true, 'placeholder' => 'Enter your password']) ?>
-                    </div>
+                    <?= form_open('/login') ?>
+                        <?= csrf_field() ?>
 
-                    <div class="d-flex justify-content-between">
-                        <?= form_submit('submit', 'Log In', ['class' => 'btn btn-primary']) ?>
-                        <a href="/signup" class="btn btn-outline-secondary">Create account</a>
-                    </div>
+                        <div class="mb-3">
+                            <?= form_label('Email address / Username', 'name-email', ['class' => 'form-label']) ?>
+                            <?= form_input ('name-email', set_value('name-email', '', false), ['id' => 'name-email', 'class' => 'form-control', 'required' => true, 'placeholder' => 'Enter your email or username'], 'text') ?>
+                        </div>
+                        
+                        <div class="mb-3">
+                            <?= form_label('Password', 'password', ['class' => 'form-label']) ?>
+                            <?= form_password('password', set_value('password', '', false), ['id' => 'password', 'class' => 'form-control', 'required' => true, 'placeholder' => 'Enter your password']) ?>
+                        </div>
 
-                    <div class="text-center mt-3">
-                        <a href="/forgot-password">Forgot password?</a>
-                    </div>
-                <?= form_close() ?>
+                        <div class="d-flex justify-content-between">
+                            <?= form_submit('submit', 'Log In', ['class' => 'btn btn-primary']) ?>
+                            <a href="/signup" class="btn btn-outline-secondary">Create account</a>
+                        </div>
+
+                        <div class="text-center mt-3">
+                            <a href="/forgot-password">Forgot password?</a>
+                        </div>
+                    <?= form_close() ?>
+                </div>
             </div>
-        </div>
-    </div>
+            <div class="col"> </div>
+        </div>    
 </div>
--- app/Views/accounts/signup.php
+++ app/Views/accounts/signup.php
@@ -1,46 +1,49 @@
-<div class="container mt-5">
-    <h2 class="text-center">Sign Up</h2>
+<div class="container mt-5 p=0 m-0">
+    <div class="row justify-content-center vw-100 vh-100 signup-bg"> 
+        <div class="col"> </div>
+        <div class="col align-self-center field-bubble">
+                <div class="m-5">
+                    <h2 class="text-center">Sign Up</h2>
+                    <div class="text-danger">
+                        <?= validation_list_errors() ?>
+                    </div>
+                
+                    <?= form_open('/signup') ?>
+                        <?= csrf_field() ?>
 
-    <div class="row justify-content-center">
-        <div class="col-md-4">
-            
-            <div class="text-danger">
-                <?= validation_list_errors() ?>
-            </div>
-        
-            <?= form_open('/signup') ?>
-                <?= csrf_field() ?>
+                        <div class="mb-3">
+                            <?= form_label('Email address', 'email', ['class' => 'form-label']) ?>
+                            <?= form_input('email', set_value('email', '', false), ['id' => 'email', 'class' => 'form-control', 'required' => true, 'placeholder' => 'Enter your email'], 'email') ?>
+                        </div>
 
-                <div class="mb-3">
-                    <?= form_label('Email address', 'email', ['class' => 'form-label']) ?>
-                    <?= form_input('email', set_value('email', '', false), ['id' => 'email', 'class' => 'form-control', 'required' => true, 'placeholder' => 'Enter your email'], 'email') ?>
+                        <div class="mb-3">
+                            <?= form_label('Username', 'username', ['class' => 'form-label']) ?>
+                            <?= form_input('username', set_value('username', '', false), ['id' => 'username', 'class' => 'form-control', 'required' => true, 'placeholder' => 'Enter your username'], 'text') ?>
+                        </div>
+
+                        <div class="mb-3">
+                            <?= form_label('Password', 'password', ['class' => 'form-label']) ?>
+                            <?= form_password('password', set_value('password', '', false), ['id' => 'password', 'class' => 'form-control', 'required' => true, 'placeholder' => 'Enter your password']) ?>
+                        </div>
+
+                        <div class="mb-3">
+                            <?= form_label('Confirm Password', 'confirm-pass', ['class' => 'form-label']) ?>
+                            <?= form_password('confirm-pass', set_value('confirm-pass', '', false), ['id' => 'confirm-pass', 'class' => 'form-control', 'required' => true, 'placeholder' => 'Confirm your password']) ?>
+                        </div>
+
+                        <div class="mb-3">
+                            <?= form_label('Date of Birth', 'birthdate', ['class' => 'form-label']) ?>
+                            <?= form_input('birthdate', set_value('birthdate', '', false), ['id' => 'birthdate', 'class' => 'form-control', 'required' => true], 'date') ?>
+                        </div>
+
+                        <div class="d-flex justify-content-between">
+                            <?= form_submit('submit', 'Create account', ['class' => 'btn btn-primary']) ?>
+                            <a href="/login" class="btn btn-outline-secondary">Log in</a>
+                        </div>
+                    <?= form_close() ?>
                 </div>
-
-                <div class="mb-3">
-                    <?= form_label('Username', 'username', ['class' => 'form-label']) ?>
-                    <?= form_input('username', set_value('username', '', false), ['id' => 'username', 'class' => 'form-control', 'required' => true, 'placeholder' => 'Enter your username'], 'text') ?>
-                </div>
-
-                <div class="mb-3">
-                    <?= form_label('Password', 'password', ['class' => 'form-label']) ?>
-                    <?= form_password('password', set_value('password', '', false), ['id' => 'password', 'class' => 'form-control', 'required' => true, 'placeholder' => 'Enter your password']) ?>
-                </div>
-
-                <div class="mb-3">
-                    <?= form_label('Confirm Password', 'confirm-pass', ['class' => 'form-label']) ?>
-                    <?= form_password('confirm-pass', set_value('confirm-pass', '', false), ['id' => 'confirm-pass', 'class' => 'form-control', 'required' => true, 'placeholder' => 'Confirm your password']) ?>
-                </div>
-
-                <div class="mb-3">
-                    <?= form_label('Date of Birth', 'birthdate', ['class' => 'form-label']) ?>
-                    <?= form_input('birthdate', set_value('birthdate', '', false), ['id' => 'birthdate', 'class' => 'form-control', 'required' => true], 'date') ?>
-                </div>
-
-                <div class="d-flex justify-content-between">
-                    <?= form_submit('submit', 'Create account', ['class' => 'btn btn-primary']) ?>
-                    <a href="/login" class="btn btn-outline-secondary">Log in</a>
-                </div>
-            <?= form_close() ?>
         </div>
+        <div class="col"> </div>
     </div>
+    
 </div>
